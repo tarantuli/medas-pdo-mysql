d68bb8c799bb7458c191abaf46c3b7a1cd1dab06
Patrick Morpey <patrick@morphp.nl>
2026-04-02 10:45:58 +0200

    Updated dependencies

--- src/Exceptions/TypeFinder.php
+++ src/Exceptions/TypeFinder.php
@@ -4,25 +4,24 @@ declare(strict_types=1);
 
 namespace Medas\PdoMysql\Exceptions;
 
-use Medas\Core\Attributes\Service;
+use Medas\Core\{Attributes\Service, Exceptions\StorageExceptionType};
 use Medas\PdoStorage\Drivers\Interfaces\ExceptionTypeFinder;
-use Medas\PdoStorage\Exceptions\ExceptionType;
 
 #[Service]
 readonly class TypeFinder implements ExceptionTypeFinder
 {
-    public function find(\Exception|\Error $e): ExceptionType
+    public function find(\Exception|\Error $e): StorageExceptionType
     {
         // PDOException carries driver-specific error info in errorInfo[1] (MySQL errno)
         $errno = $e instanceof \PDOException ? ($e->errorInfo[1] ?? null) : null;
 
         return match ($errno) {
-            1062 => ExceptionType::DuplicateKey,
-            1216, 1217, 1452 => ExceptionType::ForeignKeyViolation,
-            1213 => ExceptionType::DeadlockDetected,
-            1205 => ExceptionType::LockWaitTimeout,
-            2006, 2013 => ExceptionType::ConnectionLost,
-            default => ExceptionType::Unknown,
+            1062 => StorageExceptionType::DuplicateKey,
+            1216, 1217, 1452 => StorageExceptionType::ForeignKeyViolation,
+            1213 => StorageExceptionType::DeadlockDetected,
+            1205 => StorageExceptionType::LockWaitTimeout,
+            2006, 2013 => StorageExceptionType::ConnectionLost,
+            default => StorageExceptionType::Unknown,
         };
     }
 }
